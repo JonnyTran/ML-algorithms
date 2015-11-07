cd770c33e97f218cef695e240af59a8a26531948
JonnyTran <nhat.tran@mavs.uta.edu>
2015-11-07 14:48:28 -0600

    Added DecisionTree

--- DecisionTree/DecisionTree.py
+++ DecisionTree/DecisionTree.py
@@ -1,9 +1,20 @@
+from collections import defaultdict
+
 import numpy as np
 
 
 class DecisionTree:
+    def __init__(self):
+        pass
+
+
+class DecisionTreeClassifier:
     def __init__(self, max_level=2):
         self.max_level = max_level
+
+        def hash(): return defaultdict(hash)
+
+        self.decision_tree = hash()
 
     def train(self, trainset, class_field):
         """
@@ -11,17 +22,23 @@ class DecisionTree:
         :param dataset: a panda dataframe object
         :param class_field: a string of the name of the field containing class
         """
+
+        ##### Initialize dataset variables #####
         self.trainset = trainset
         self.input_size = len(trainset)
         self.classes = list(np.unique(trainset[class_field]))
         self.n_classes = len(self.classes)
         self.split_fields = list(self.trainset.columns - [class_field])
 
-        for i in range(self.max_level):
-            # Find best split
-            pass
+        ##### Initialize Decision Tree #####
 
-    def computeEntropy(self):
+
+        for level in range(self.max_level):
+            # Find best split
+            for field in self.split_fields:
+                values = list(np.unique(trainset[field]))
+
+    def computeEntropy(self, trainset, conditions, class_field, n_classes):
         pass
 
     def split(self):
