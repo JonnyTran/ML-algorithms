b55443f8a7ac880f1b7409c2b410f748cd0a5649
JonnyTran <nhat.tran@mavs.uta.edu>
2015-12-04 21:48:14 -0600

    SemiSupervised: Logistic Self Learning

--- SemiSupervised/LogisticSelfLearning.py
+++ SemiSupervised/LogisticSelfLearning.py
@@ -1,24 +1,29 @@
-from ML_Algorithms.Regression import LogisticRegression
-import pandas as pd
-import numpy as np
-
 class SelfLearner:
-    def __init__(self, learning_algorithm):
-        pass
+    def __init__(self, learner):
+        self.learner = learner
 
-    def train(self, labeled_set, unlabeled_set, k_samples=5):
+    def train(self, labeled_set, unlabeled_set , k_samples=5):
         self.labeled_set = labeled_set
         self.unlabeled_set = unlabeled_set
 
+    def label_top_k(self):
+        pass
 
-
-
-
-    def label_topk(self):
+    def predict(self, tuple):
         pass
 
     def test(self):
         pass
 
 if __name__ == '__main__':
-    pd.read_csv('../Decision')
+    W = "W"
+    M = "M"
+    train_labeled = [((170, 57, 32), W), ((190, 95, 28), M), ((150, 45, 35), W), ((168, 65, 29), M), ((175, 78, 26), M),
+                     ((185, 90, 32), M), ((171, 65, 28), W), ((155, 48, 31), W), ((165, 60, 27), W)]
+    train_unlabeled = [(182, 80, 30), (175, 69, 28), (178, 80, 27), (160, 50, 31), (170, 72, 30), (152, 45, 29),
+                       (177, 79, 28), (171, 62, 27), (185, 90, 30), (181, 83, 28), (168, 59, 24), (158, 45, 28),
+                       (178, 82, 28), (165, 55, 30), (162, 58, 28), (180, 80, 29), (173, 75, 28), (172, 65, 27),
+                       (160, 51, 29), (178, 77, 28), (182, 84, 27), (175, 67, 28), (163, 50, 27), (177, 80, 30),
+                       (170, 65, 28)]
+    test = [((169, 58, 30), W), ((185, 90, 29), M), ((148, 40, 31), W), ((177, 80, 29), M), ((170, 62, 27), W),
+            ((172, 72, 30), M), ((175, 68, 27), W), ((178, 80, 29), M)]
