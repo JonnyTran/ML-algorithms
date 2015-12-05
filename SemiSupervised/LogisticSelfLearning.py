b8ecf7f7f2651a645c2ade0adf3618b50ed336ce
JonnyTran <nhat.tran@mavs.uta.edu>
2015-12-04 19:58:57 -0600

    moved things around

---
+++ SemiSupervised/LogisticSelfLearning.py
@@ -0,0 +1,24 @@
+from ML_Algorithms.Regression import LogisticRegression
+import pandas as pd
+import numpy as np
+
+class SelfLearner:
+    def __init__(self, learning_algorithm):
+        pass
+
+    def train(self, labeled_set, unlabeled_set, k_samples=5):
+        self.labeled_set = labeled_set
+        self.unlabeled_set = unlabeled_set
+
+
+
+
+
+    def label_topk(self):
+        pass
+
+    def test(self):
+        pass
+
+if __name__ == '__main__':
+    pd.read_csv('../Decision')
