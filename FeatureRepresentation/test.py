1187274211cbc006509e2336771b785084c08c3c
Nhat Tran <nhat.tran@mavs.uta.edu>
2016-06-01 03:59:07 -0500

    Update test.py

--- FeatureRepresentation/test.py
+++ FeatureRepresentation/test.py
@@ -2,8 +2,13 @@ import numpy as np
 
 from FeatureRepresentation.SparseRepresentation import KSVDSparseCoding
 
-X = np.random.rand(50, 40)
+X = np.random.rand(100, 100)
 n_samples, n_features = X.shape
 
-sr = KSVDSparseCoding(n_components=100, verbose=True)
+sr = KSVDSparseCoding(n_components=200, max_iter=300, verbose=1)
 sr.fit(X)
+
+errs = X.T - np.dot(sr.dictionary, sr.code)
+sample_errs = [np.linalg.norm(errs[:, n]) for n in range(n_samples)]
+print max(sample_errs)
+print np.linalg.norm(errs, ord="fro")
