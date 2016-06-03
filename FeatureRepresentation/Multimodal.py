2e65a24d09610e2199e54acdf69fca715bdf4a12
JonnyTran <nhat.tran@mavs.uta.edu>
2016-06-03 06:12:55 -0500

    Multimodal: TODO need to have multimodal atoms normalized

--- FeatureRepresentation/Multimodal.py
+++ FeatureRepresentation/Multimodal.py
@@ -16,26 +16,29 @@ class Multimodal():
         features_indx = np.cumsum(self.n_features)
         for i in range(self.n_modals):
             if i == 0:
-                self.dictionaries.append(self.sparse_coder.dictionary[0:features_indx[i] - 1, :])
+                self.dictionaries.append(
+                    self.sparse_coder.dictionary[0:features_indx[i], :] * self.n_features[i] ** 0.5)
             else:
                 a = features_indx[i - 1]
-                b = features_indx[i] - 1
-                self.dictionaries[i].append(self.sparse_coder.dictionary[a:b, :])
+                b = features_indx[i]
+                self.dictionaries.append(
+                    self.sparse_coder.dictionary[a:b, :] * self.n_features[i] ** 0.5)
 
     def initialize(self, X_s):
         self.n_samples = X_s[0].shape[0]
         self.n_features = [0, ] * len(X_s)
         for i in range(len(X_s)):
             self.n_features[i] = X_s[i].shape[1]
+            X_s[i] /= self.n_features[i] ** 0.5  # Scale constant
 
         print "X_s: n_samples", self.n_samples, ", n_features", self.n_features
         self.X_s = np.concatenate(X_s, axis=1)
 
     def get_dictionary(self, modal_no):
-        return self.dictionaries[modal_no] * self.n_features[modal_no] ** 0.5
+        return self.dictionaries[modal_no]
 
     def sparse_encode(self, X, modal_no):
-        pass
+        return self.sparse_coder.sparse_encode(X, self.dictionaries[modal_no])
 
 
 def main():
@@ -51,12 +54,13 @@ def main():
     X0[:, 0:10] += 0.3 * X1[:, 0:10] + 0.1 * (np.random.rand(n_samples, 10) - 0.5)
     X1[:, 0:10] += X0[:, 0:10] + 0.1 * (np.random.rand(n_samples, 10) - .05)
 
-    mm = Multimodal(n_modals=2, sparse_coder=KSVDSparseCoding(n_components=n_components, max_iter=25, verbose=1))
+    mm = Multimodal(n_modals=2, sparse_coder=KSVDSparseCoding(n_components=n_components, max_iter=5, verbose=1))
     mm.fit([X0, X1])
 
     for dic in mm.dictionaries:
         print dic.shape
-        print dic
+        print "dic[:,0] norm", np.linalg.norm(dic[:, 0])
+        print dic[:, 0]
 
 
 if __name__ == "__main__":
