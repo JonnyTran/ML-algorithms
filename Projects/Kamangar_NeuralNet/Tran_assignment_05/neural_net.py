2dacc80a3df8bd9a6c4d7034b863fdb157b88b2c
JonnyTran <nhat.tran@mavs.uta.edu>
2016-11-15 15:19:29 -0600

    nnet assignment 5

--- Projects/Kamangar_NeuralNet/Tran_assignment_05/neural_net.py
+++ Projects/Kamangar_NeuralNet/Tran_assignment_05/neural_net.py
@@ -3,64 +3,94 @@
 # 2016-11-15
 # Assignment_05
 
+import numpy as np
 import theano
 from theano import tensor as T
-import numpy as np
 
 
 def floatX(X):
     """
     Used to convert np arrays into theano matrices
-    :param X:
-    :return:
     """
     return np.asarray(X, dtype=theano.config.floatX)
 
 
 class NeuralNet:
-    def __init__(self, n_inputs=768, n_classes=10, n_hidden_nodes=100, alpha=0.1, n_epoch=200, activation='sigmoid'):
+    def __init__(self, n_inputs=1024, n_classes=10, n_hidden_nodes=100, alpha=0.1, lr=0.05, n_epoch=200,
+                 activation='sigmoid'):
         """
         A neural network implementation using Theano for a one-hidden layer and output layer with 10 nodes
 
-        :param n_hidden_nodes: Number of nodes in the hidden layer
-        :param alpha: the coefficient for L-2 weight regularization
-        :param n_epoch: Number of training epochs for SGD. Default: 200
-        :param activation: Choice of activation method among ['sigmoid', 'softmax', 'relu']. Default: 'sigmoid'
-        :param n_inputs: number of inputs (hard coded for assignment)
-        :param n_classes: number of output nodes (hard coded for assignment)
+        :param n_hidden_nodes:
+            Number of nodes in the hidden layer
+        :param alpha:
+            the coefficient for L-2 weight regularization
+        :param n_epoch:
+            Number of training epochs for SGD. Default: 200
+        :param activation:
+            Choice of activation method among ['sigmoid', 'relu', 'linear']. Default: 'sigmoid'
+        :param n_inputs:
+            number of inputs (hard coded for assignment)
+        :param n_classes:
+            number of output nodes (hard coded for assignment)
         """
         self.activation = activation
         self.n_epoch = n_epoch
-        self.alpha = alpha
         self.n_hidden_nodes = n_hidden_nodes
         self.n_inputs = n_inputs
         self.n_classes = n_classes
 
-        self.layers = [theano.shared(floatX(np.random.rand((self.n_inputs, self.n_hidden_nodes)))),
-                       theano.shared(floatX(np.random.rand((self.n_hidden_nodes, self.n_classes))))]
+        # Initialize Weights & Theano variables & symbolic equations
+        X = T.fmatrix('X')
+        y = T.fmatrix('y')
+
+        self.layers = [
+            theano.shared(name="W_hidden", value=floatX(np.random.rand(self.n_inputs, self.n_hidden_nodes) - 0.5)),
+            theano.shared(name="W_output", value=floatX(np.random.rand(self.n_hidden_nodes, self.n_classes) - 0.5))]
+
+        self.lr = theano.shared(floatX(lr))
+        self.alpha = theano.shared(floatX(alpha))
+
+        if self.activation == 'sigmoid':
+            self.fprop = T.dot(T.nnet.sigmoid(T.dot(X, self.layers[0])), self.layers[1])
+        elif self.activation == 'relu':
+            self.fprop = T.dot(T.nnet.relu(T.dot(X, self.layers[0])), self.layers[1])
+        else:
+            self.fprop = T.dot(T.dot(X, self.layers[0]), self.layers[1])
+
+        self.regularization = 0.5 * self.alpha * T.dot(self.layers[0].T, self.layers[0]) + \
+                              0.5 * self.alpha * T.dot(self.layers[1].T, self.layers[1])
+
+        self.loss = T.mean(T.nnet.softmax(self.fprop(X)) - y) + self.regularization
+
+        gradient_hidden = T.grad(cost=self.loss, wrt=self.layers[0])
+        gradient_output = T.grad(cost=self.loss, wrt=self.layers[1])
+        self.update = [(self.layers[0], self.layers[0] - gradient_hidden * self.lr),
+                       (self.layers[1], self.layers[1] - gradient_output * self.lr)]
+
+        self.predict = T.argmax(self.fprop(X), axis=1)
 
     def fprop(self, X):
         pass
 
-    def loss(self, X):
-        if self.activation == 'sigmoid':
-            return T.nnet.sigmoid(X)
-        elif self.activation == 'softmax':
-            return T.nnet.softmax(X)
-        elif self.activation == 'relu':
-            return T.nnet.relu(X)
-        else:
-            return X
+    def loss(self):
+        pass
+
+    def regularization(self):
+        pass
 
     def bprop(self):
         pass
 
     def fit(self, X, y):
-        pass
+        return theano.function(inputs=[X, y], outputs=self.loss, updates=self.update)
 
-    def predict(self, X):
-        pass
+    def predict(self):
+        return theano.function(inputs=[X], outputs=self.predict)
 
 
 if __name__ == "__main__":
-    pass
+    X = np.random.rand(100, 1024 * 3)
+    y = np.eye(100)
+    nnet = NeuralNet(n_inputs=1024 * 3, n_hidden_nodes=100, alpha=0.1, n_epoch=200, activation='sigmoid')
+    print nnet.fit(X, y)
